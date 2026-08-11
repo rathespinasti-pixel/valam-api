f2b1f32827d939f0d34f36f15ac251f788022003
Pinastina <rathespinasti@gmail.com>
2026-08-11 11:41:39 +0530

    update CORS configuration to restrict origins to localhost

--- app/config.py
+++ app/config.py
@@ -64,7 +64,7 @@ class Config:
 
     # CORS
     # CORS
-    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:3000")
 
     # Swagger
     SWAGGER = {
